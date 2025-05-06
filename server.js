a200ba7bbcecb7771fc0139af3c1a84dcef0d5be
Siddhartha Borah <siddhartha.borah@outlook.com>
2025-05-07 00:30:24 +0530

    test route

--- server.js
+++ server.js
@@ -86,3 +86,8 @@ app.use((err, req, res, next) => {
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
